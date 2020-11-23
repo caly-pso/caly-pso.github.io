89ac632214dc45c4cd6cac6395f9cde0b614a7d7
Calypso Rynkowski <calypso.rynkowski@gmail.com>
2020-11-23 16:15:32 -0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+!(function ($) {
+  "use strict";
+
+  //filter
+  $(window).on("load", function () {
+    var portfolioIsotope = $(".portfolio-container").isotope({
+      itemSelector: ".portfolio-item",
+      layoutMode: "fitRows",
+    });
+
+    $("#portfolio-fltrs li").on("click", function () {
+      $("#portfolio-flters li").removeClass("filter-active");
+      $(this).addClass("filter-active");
+
+      portfolioIsotope.isotope({
+        filter: $(this).data("filter"),
+      });
+    });
+  });
+
+  //Pictures
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    loop: true,
+    autoplaySpeed: 1000,
+    items: 1,
+    autoplay: true,
+  });
+
+  // Back To Top
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+
+  $(".back-to-top").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500,
+      "easeInOutExpo"
+    );
+    return false;
+  });
+})(jQuery);
